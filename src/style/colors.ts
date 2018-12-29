ed93b592ad77e2c6eb13651d805279227b7aa0bf
kingdaro <kingdaro@gmail.com>
2018-12-28 22:22:34 -0600

    back to navy, it looks nice

--- src/style/colors.ts
+++ src/style/colors.ts
@@ -1,9 +1,9 @@
-import { hsl, shade } from "polished"
+import { shade } from "polished"
 
 const navy = "rgb(39, 60, 117)"
 const white = "rgb(245, 246, 250)"
 
-export const primaryColor = hsl(0, 0, 0.2)
+export const primaryColor = navy
 export const primaryAltColor = shade(0.15, primaryColor)
 export const primaryTextColor = white
 export const primaryBackgroundColor = shade(0.3, primaryColor)
